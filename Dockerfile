fac97e466d7be296e57c7611c125f21534452f89
Tan Wei Lian <tanweilian@outlook.sg>
2020-10-15 21:28:42 +0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+# pull official base image
+FROM node:13.12.0-alpine as fe
+
+# set working directory
+WORKDIR /app
+
+# add `/app/node_modules/.bin` to $PATH
+ENV PATH /app/node_modules/.bin:$PATH
+
+# install app dependencies
+COPY ./book_frontend/package.json ./book_frontend/yarn.lock ./
+
+RUN yarn install --silent 
+
+COPY ./book_frontend/ ./
+
+RUN yarn build
+
+
+#backend
+FROM python:3.8.3-alpine
+
+WORKDIR /usr/src/app
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+RUN pip install --upgrade pip
+COPY ./book_site/requirements.txt .
+
+RUN \
+ apk add --no-cache bash && \
+ apk update && \
+ apk add --no-cache postgresql-libs && \
+ apk add --no-cache jpeg-dev zlib-dev && \
+ apk add --no-cache --virtual .build-deps gcc musl-dev postgresql-dev && \
+ python3 -m pip install -r requirements.txt --no-cache-dir && \
+ apk --purge del .build-deps
+
+RUN pip install -r requirements.txt
+
+COPY ./book_site/ ./
+RUN chmod +x ./wait-for-it.sh
+COPY --from=fe ./app/build ./frontend/build
---
+++ book_site/frontend/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+import logging
+import urllib.request
+import os
+
+# Create your views here.
+class FrontendAppView(View):
+    """
+    Serves the compiled frontend entry point (only works if you have run `yarn
+    run build`).
+    """
+    def get(self, request):
+            print (os.path.join(settings.REACT_APP_DIR, 'build', 'index.html'))
+            try:
+                with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
+                    return HttpResponse(f.read())
+            except FileNotFoundError:
+                logging.exception('Production build of app not found')
+                return HttpResponse(
+                    """
+                    This URL is only used when you have built the production
+                    version of the app. Visit http://localhost:3000/ instead, or
+                    run `yarn run build` to test the production version.
+                    """,
+                    status=501,
+                )
