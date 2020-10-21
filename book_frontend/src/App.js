8d6ca651c33fb2099cc5f2a74564ed1455ce12ec
Raymond Djajalaksana <ray-cap@hotmail.com>
2020-10-21 21:58:40 +0800

    add chatbot ui framework

--- book_frontend/src/App.js
+++ book_frontend/src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React , {useEffect } from 'react';
 import './App.css';
 import Blog from './blog/Blog';
+import { Widget , addResponseMessage } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
+import logo from './logo.svg';
+
+import './chatbot_style.css';
 
 function App() {
+    useEffect(() => {
+        addResponseMessage('Welcome to our page! Feel free to chat with me about book.');
+    }, []);
+    
+    const handleNewUserMessage = (newMessage) => {
+        var replied = `your message is: ${newMessage}`
+        // TODO: need to send to backend
+        addResponseMessage(replied);
+    };
   return (
-    <Blog />
+    <div className="App">
+      <Blog />
+      <Widget 
+        handleNewUserMessage={handleNewUserMessage}
+        profileAvatar={logo}
+        titleAvatar={logo}
+        title="Book Chatbot"
+        subtitle="please be patient..."
+        showTimeStamp={false}
+      />
+    </div>
   );
 }
 
